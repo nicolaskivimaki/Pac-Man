d1c3958259a96cea1ab66259e9a7da1cc7f091be
Nicolas Kivimäki <kivimakinicolas@gmail.com>
2022-03-28 21:06:25 +0300

    lataa_rahaa

--- laskarit/viikko2/maksukortti/src/tests/maksukortti_test.py
+++ laskarit/viikko2/maksukortti/src/tests/maksukortti_test.py
@@ -23,4 +23,12 @@ class TestMaksukortti(unittest.TestCase):
         self.kortti.syo_edullisesti()
         self.assertEqual(str(self.kortti), "Kortilla on rahaa 2 euroa")
 
+    def test_kortille_voi_ladata_rahaa(self):
+        self.kortti.lataa_rahaa(25)
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 35 euroa")
+
+    def test_kortin_saldo_ei_ylita_maksimiarvoa(self):
+        self.kortti.lataa_rahaa(200)
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 150 euroa")
+
 
