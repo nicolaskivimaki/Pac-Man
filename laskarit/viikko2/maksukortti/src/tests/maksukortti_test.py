ac461b2c1570a36bed3e5dab0bc8cb8a7a09eb16
Nicolas Kivimäki <kivimakinicolas@gmail.com>
2022-03-28 21:03:59 +0300

    setup

--- laskarit/viikko2/maksukortti/src/tests/maksukortti_test.py
+++ laskarit/viikko2/maksukortti/src/tests/maksukortti_test.py
@@ -2,20 +2,25 @@ import unittest
 from maksukortti import Maksukortti
 
 class TestMaksukortti(unittest.TestCase):
-	def setUp(self):
-		print("Set up goes here")
+    def setUp(self):
+        self.kortti = Maksukortti(10)
 
-	def test_konstruktori_asettaa_saldon_oikein(self):
-		kortti = Maksukortti(10)
+    def test_konstruktori_asettaa_saldon_oikein(self):
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 10 euroa")
 
-		vastaus = str(kortti)
-		self.assertEqual(vastaus, "Kortilla on rahaa 10 euroa")
+    def test_syo_edullisesti_vahentaa_saldoa_oikein(self):
+        self.kortti.syo_edullisesti()
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 7.5 euroa")
 
-	def test_syo_edullisesti_vahentaa_saldoa_oikein(self):
-		kortti = Maksukortti(10)
+    def test_syo_maukkaasti_vahentaa_saldoa_oikein(self):
+        self.kortti.syo_maukkaasti()
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 6 euroa")
 
-		kortti.syo_edullisesti()
 
-		self.assertEqual(str(kortti), "Kortilla on rahaa 7.5 euroa")
+    def test_syo_edullisesti_ei_vie_saldoa_negatiiviseksi(self):
+        self.kortti.syo_maukkaasti()
+        self.kortti.syo_maukkaasti()
+        self.kortti.syo_edullisesti()
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 2 euroa")
 
 
