8f6f71890cc11b6a767c984f2daeaeed3853b104
Nicolas Kivimäki <kivimakinicolas@gmail.com>
2022-04-25 17:34:32 +0300

    siirto

---
+++ pacman-app/src/events.py
@@ -0,0 +1,5 @@
+import pygame
+
+class Events:
+    def get(self):
+        return pygame.event.get()
---
+++ pacman-app/src/index.py
@@ -0,0 +1,13 @@
+import  pygame 
+from pelipyorii import PeliPyorii
+from peli import Peli
+from events import Events
+def main():
+    alusta = pygame.display.set_mode((1000, 1000))
+    peli = Peli(alusta)
+    events = Events()
+    peli_pyorii = PeliPyorii(peli, alusta, events)
+    pygame.init()
+    peli_pyorii.start()
+if __name__ == "__main__":
+    main()
