f9e9f62dd88b101e4c93d7b0b07963cbb71b710c
Nicolas Kivimäki <kivimakinicolas@gmail.com>
2022-05-14 22:40:49 +0300

    updates

--- pacman-app/src/ominaisuudet/peli.py
+++ pacman-app/src/ominaisuudet/peli.py
@@ -5,6 +5,7 @@ from ominaisuudet.level import *
 from ominaisuudet.events import Events
 from ominaisuudet.pelipyorii import *
 from sprites.pacman import Pacman
+from sprites.ghosts import Ghosts
 
 dirname = os.path.dirname(__file__)
 
@@ -22,6 +23,7 @@ class Game:
         self._level = Level(self._screen)
         self._events = Events()
         self._pacman = Pacman(self._speed)
+        self._ghosts = Ghosts(self._speed)
         self._event_handler = HandleEvents(self._events)
 
     def _run_game(self):
@@ -32,6 +34,7 @@ class Game:
     def _start_game(self):
         self._level._create_level()
         self._screen.blit(self._pacman._pacman, (self._pacman.rect.x, self._pacman.rect.y))
+        self._screen.blit(self._ghosts._ghosts, (self._ghosts.rect.x, self._ghosts.rect.y))
         pygame.display.update()
 
     def _move_pacman(self, direction, opposite=None):
@@ -76,6 +79,7 @@ class Game:
         self._level._draw_level()
         self._pacman_can_move()
         self._screen.blit(self._pacman._pacman, (self._pacman.rect.x, self._pacman.rect.y))
+        self._screen.blit(self._ghosts._ghosts, (self._ghosts.rect.x, self._ghosts.rect.y))
 
     def _game_screen(self):
         self._start_game()
--- pacman-app/src/sprites/ghosts.py
+++ pacman-app/src/sprites/ghosts.py
@@ -5,11 +5,11 @@ dirname = os.path.dirname(__file__)
 class Ghosts(pygame.sprite.Sprite):
 
     def __init__(self, speed):
-        self._ghost = pygame.image.load(os.path.join(dirname, "..", "assets", "ghost.png"))
-        self._pacman = pygame.transform.smoothscale(self._pacman, (30, 30))
+        self._ghosts = pygame.image.load(os.path.join(dirname, "..", "assets", "ghost.png"))
+        self._ghosts = pygame.transform.smoothscale(self._ghosts, (30, 30))
         self._speed = speed
-        self.rect = self._pacman.get_rect()
-        self.rect.x = 400
-        self.rect.y = 400
+        self.rect = self._ghosts.get_rect()
+        self.rect.x = 600
+        self.rect.y = 625
         self._direction = 0
         
